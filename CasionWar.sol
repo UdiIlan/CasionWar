25d46683972710d6710eb6f76a8d005f0cdea2e1
UdiIlan <37189527+UdiIlan@users.noreply.github.com>
2018-06-04 23:24:13 +0300

    Update CasionWar.sol

--- CasionWar.sol
+++ CasionWar.sol
@@ -9,9 +9,58 @@ contract CasinoWar {
     address player1;
     address player2;
     
+    uint player1_payment;
+    
+    bytes32 player1_hash;
+    bytes32 player2_hash;
+
+    bool player1_set_hash;
+    bool player2_set_hash;
     
     function CasinoWar() public
     {
         curr_state = WAITING_FOR_PLAYER_1;
+        player1_set_hash = false;
+        player2_set_hash = false;
+    }
+    
+    function StartGame() public payable
+    {
+        require(curr_state == WAITING_FOR_PLAYER_1);
+        require(msg.value > 0);
+        player1 = msg.sender;
+        player1_payment = msg.value;
+        curr_state = WAITING_FOR_PLAYER_2;
+    }
+    
+    function JoinGame() public payable
+    {
+        require(curr_state == WAITING_FOR_PLAYER_2);
+        require(msg.value == player1_payment);
+        require(player1 != msg.sender)
+        player2 = msg.sender;
+        curr_state = WAITING_FOR_HASHES;
+    }
+    
+    function SetHash(bytes32 player_hash)
+    {
+        require(curr_state == WAITING_FOR_HASHES && (msg.sender == player1 && 
+                !player1_set_hash || msg.sender == player2 && 
+                !player2_set_hash));
+        if (msg.sender == player1)
+        {
+            player1_set_hash = true;
+            player1_hash = player_hash;
+        }
+        else if (msg.sender == player2)
+        {
+            player2_set_hash = true;
+            player2_hash = player_hash;
+        }
+        
+        if (player1_set_hash && player2_set_hash)
+        {
+            curr_state = WAITING_FOR_SEEDS;
+        }
     }
 }
